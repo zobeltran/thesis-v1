f5b25446b00a63f92393e2b358a79690d99bae4f
Renzo Beltran <rnzbeltran@gmail.com>
2018-07-02 13:19:20 +0800

    Added new forms for new models

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,8 @@ from wtforms import StringField, PasswordField, BooleanField, SelectField
 from wtforms import IntegerField, TextAreaField, FormField, FieldList
 from wtforms.fields.html5 import DateField, TimeField
 from wtforms.validators import InputRequired, Length, EqualTo, Email
+from wtforms.validators import NumberRange, ValidationError
+from datetime import date
 
 csrf = CSRFProtect()
 # Validation Messages
@@ -13,13 +15,31 @@ passwordLength = ('Password has a minimum of 8 and a maximum '
                   'of 100 characters.')
 firstNameLength = ('First Name has a maximum of 100 characters.')
 lastNameLength = ('Last Name has a maximum of 100 characters.')
+contactnoLength = ('Contact Number must be at least 7 digits')
 # Register Ticket Validations
 originLength = ('Origin has a maximum of 50 characters.')
 arrivalLength = ('Arrival has a maximum of 50 characters.')
+numberAdult = ('Number of Adult must be at least 1')
 # Register Hotel Validations
 hotelNameLength = ('Name has a maximum of 100 characters.')
 roomTypeLength = ('Room Type has a maximum of 100 characters')
 roomDetailsLength = ('Room Details has maximum of 300 characters')
+# Inquire Hotel Validations
+numberGuest = ('Number of Guest must be at least 1')
+budget = ('Budget must be at lease 500 pesos')
+
+
+def DateCheck(form, field):
+    if field.data < date.today():
+        raise ValidationError(('%s must not be past '
+                               'today' % (field.label.text)))
+
+
+def AfterDateCheck(form, field, fieldTwo):
+    if field.data > fieldTwo.data:
+        raise ValidationError(field.label +
+                              (' must not be before '
+                               + fieldTwo.label))
 
 
 class LoginForm(FlaskForm):
@@ -69,9 +89,12 @@ class RegisterTicket(FlaskForm):
                               [InputRequired('Departure Time is Required')])
     arrivalDate = DateField('Arrival Date',
                             [InputRequired('Arrival Date is Required')])
-    arrivalTime = TimeField('Arrival Time', [])
+    arrivalTime = TimeField('Arrival Time',
+                            [InputRequired('Arrival Time is Required')])
     slots = IntegerField('Slots',
                          [InputRequired('Slots are Required')])
+    price = IntegerField('Price',
+                         [InputRequired('Price is Required')])
     isPackaged = BooleanField('Packaged')
 
 
@@ -87,12 +110,14 @@ class RegisterHotel(FlaskForm):
     details = TextAreaField('Room Details',
                             [InputRequired('Room Details is Required'),
                              Length(max=300, message=roomDetailsLength)])
-    stayDays = IntegerField('Days of Stay',
-                            [InputRequired('Days of Stay is Required')])
-    stayNights = IntegerField('Nights of Stay',
-                              [InputRequired('Nights of Stay is Required')])
+    checkIn = DateField('Check In Date',
+                        [InputRequired('CheckIn Date is Required')])
+    checkOut = DateField('Check Out Date',
+                         [InputRequired('Check Out Date is Required')])
     expirationDate = DateField('Expiration Date',
                                [InputRequired('Expiration Date is Required')])
+    price = IntegerField('Price',
+                         [InputRequired('Price is Required')])
     isPackaged = BooleanField('Packaged')
 
 
@@ -103,7 +128,78 @@ class RegisterCustomerFlightsFields(FlaskForm):
     lastName = StringField('Last Name',
                            [InputRequired('Last Name is Required'),
                             Length(max=100, message=lastNameLength)])
+    email = StringField('Email',
+                        [InputRequired('Email is Required'),
+                         Email('Not a valid Email')])
+    contactNo = IntegerField('Contact Number',
+                             [InputRequired('Contact Number is Required'),
+                              Length(min=7, message=contactnoLength)])
 
 
 class RegisterCustomerFlights(FlaskForm):
     customer = FieldList(FormField(RegisterCustomerFlightsFields))
+
+
+class InquiryFlights(FlaskForm):
+    firstName = StringField('First Name',
+                            [InputRequired('First Name is Required'),
+                             Length(max=100, message=firstNameLength)])
+    lastName = StringField('Last Name',
+                           [InputRequired('Last Name is Required'),
+                            Length(max=100, message=lastNameLength)])
+    email = StringField('Email',
+                        [InputRequired('Email is Required'),
+                         Email('Not a valid Email')])
+    origin = StringField('Origin Location',
+                         [InputRequired('Origin Location is Required'),
+                          Length(max=100, message=originLength)])
+    arrival = StringField('Arrival Location',
+                          [InputRequired('Arrival Location is Required'),
+                           Length(max=100, message=arrivalLength)])
+    departureDate = DateField('Departure Date',
+                              [InputRequired('Departure Date is Required')])
+    arrivalDate = DateField('Arrival Date',
+                            [InputRequired('Arrival Date is Required')])
+    time = SelectField('Desired Time',
+                       choices=[('AM', 'AM'),
+                                ('PM', 'PM')])
+    adult = IntegerField('Number of Adults',
+                         [InputRequired('Number is Required'),
+                          NumberRange(min=1, message=numberAdult)],
+                         default=1)
+    child = IntegerField('Number of Child',
+                         [InputRequired('Number is Required')],
+                         default=0)
+    infant = IntegerField('Number of Infant(Below 2 years old)',
+                          [InputRequired('Number is Required')],
+                          default=0)
+    note = TextAreaField('Note')
+
+
+class InquiryHotels(FlaskForm):
+    firstName = StringField('First Name',
+                            [InputRequired('First Name is Required'),
+                             Length(max=100, message=firstNameLength)])
+    lastName = StringField('Last Name',
+                           [InputRequired('Last Name is Required'),
+                            Length(max=100, message=lastNameLength)])
+    email = StringField('Email',
+                        [InputRequired('Email is Required'),
+                         Email('Not a valid Email')])
+    location = StringField('Location',
+                           [InputRequired('Please input a location')])
+    budget = IntegerField('Budget Range',
+                          [InputRequired('Input Budget Range'),
+                           NumberRange(min=500, message=budget)],
+                          default=500)
+    guest = IntegerField('Number of Guest',
+                         [InputRequired('Input Number of Guest'),
+                          NumberRange(min=1, message=numberGuest)],
+                         default=1)
+    checkIn = DateField('Check-in Date',
+                        [InputRequired('Check-in Date is Required'),
+                         DateCheck])
+    checkOut = DateField('Check-out Date',
+                         [InputRequired('Check-out Date is Required'),
+                          DateCheck])
+    note = TextAreaField('Note')
